a5f2b775ea1ac6282f5e2afa7aa32f27d330b4f8
Auggus <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2023-06-15 09:58:40 +0200

    Update wrapper-virt2-v2.0.py

--- wrapper-virt2-v2.0.py
+++ wrapper-virt2-v2.0.py
@@ -104,7 +104,7 @@ def perform_action(option,number,attacker,pentest,index_attacker,index_pentest,c
 def show_ip(ip):
     if ip == "Y":
         print(' >>> Loading IPs <<< ')
-        #print(f"[ Username : root ; Password : Pa$$w0rd ]")
+        print(f"[ Username : root ; Password : Pa$$w0rd ]")
         output = os.popen('lxc-ls -f').read()
         pattern = r"(\S+)\s+RUNNING\s+\S+\s+-\s+(\S+)"
         matches = re.findall(pattern, output)
